40993ce58edef0d8371ed1c56b4f9d6c93f061ef
Nguyễn Sơn Minh <minh23923@gmail.com>
2024-12-11 21:55:55 +0700

    session14_bai2

---
+++ session14_bai2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h> 
+
+int main(){
+	char str[]="Nguyen Son Minh";
+	int size=strlen(str);
+	for(int i=0; i<size; i++){
+		printf("%c ",str[i]);
+	}
+		
+
+
+
+return 0;
+}
+
