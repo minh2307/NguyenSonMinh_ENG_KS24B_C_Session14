a080f673638b1e1a423d7195874ffe2c3b7f5514
Nguyễn Sơn Minh <minh23923@gmail.com>
2024-12-13 00:12:23 +0700

    session14_bai10

---
+++ session14_bai10.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include <string.h>
+int main(){
+	char text[50]="Nguyen Son Minh";
+	int count=0;
+	int size=strlen(text); 
+	char word[50]; 
+	int a=0; 
+	int number[50]={0}; 
+	word[a]=text[0] ; 
+	for(int i=1;i<size;i++){
+		int flag=-1; 
+		for(int j=0;j<=a;j++){
+			if(word[j]==text[i]){
+				flag=1; 
+				break; 
+			} 
+		} 
+		if(flag!=1){
+				a++; 
+				word[a]=text[i];
+			} 
+	} 
+	for (int i = 0; i < size; i++) {
+        for (int j = 0; j <= a; j++) {
+            if (text[i] == word[j]) {
+                number[j]++;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i <= a; i++) {
+        printf("ki tu '%c' xuat hien %d lan\n", word[i], number[i]);
+    }
+	return 0; 
+} 
